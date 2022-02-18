a58746029658ccaa3f87b85a6476a78b3dc7c434
viper23 <htonnycarlos@gmail.com>
2022-02-18 20:01:24 +0200

    added components for skills section

---
+++ cv/src/components/knowledges/Languages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Languages = () => {
+    return (
+        <div>
+            Languages et frameWork
+        </div>
+    );
+};
+
+export default Languages;
---
+++ cv/src/components/knowledges/OtherSkills.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OtherSkills = () => {
+    return (
+        <div>
+            OtherSkills
+        </div>
+    );
+};
+
+export default OtherSkills;
