b2353e081dea2a4343155142ac74c8f1fe59c134
viper23 <htonnycarlos@gmail.com>
2022-02-18 11:01:23 +0200

    finish the navigation bar

---
+++ cv/src/components/Navigation.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div className='sidebar'>
+            
+            <div className='id'>
+                <div className='idContent'>
+                    <img src='./media/billGates.jpg' alt='profile_picture'/>
+                    <h3>Hakizimana Tony Carlin</h3>
+                </div>
+            </div>
+
+            <div className='navigation'>
+                <ul>
+                    <li>
+                        <NavLink exact to = "/" activeClassName='navActive'>
+                            <i className='fas fa-home'></i>
+                            <span>Home</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to = "/skills" activeClassName='navActive'>
+                            <i className='fas fa-mountain'></i>
+                            <span>Skills</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to = "/portfolio" activeClassName='navActive'>
+                            <i className='fas fa-images'></i>
+                            <span>Portfolio</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to = "/contact" activeClassName='navActive'>
+                            <i className='fas fa-address-book'></i>
+                            <span>Contact</span>
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+
+            <div className='socialNetwork'>
+                <ul>
+                    <li>
+                        <a href="https://www.twitter.com/tonyv23_" target="_blank" rel= "noopener noreferrer">
+                            <i className="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/tonyv23_" target="_blank" rel= "noopener noreferrer">
+                            <i className="fab fa-instagram"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.fr/tonyv23_" target="_blank" rel= "noopener noreferrer">
+                            <i className="fab fa-linkedin"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.github.com/tonyv23_" target="_blank" rel= "noopener noreferrer">
+                            <i className="fab fa-github"></i></a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className='signature'>
+                <p>Viper23 - 2022</p>
+            </div>
+        
+        </div>
+    );
+};
+
+export default Navigation;
--- cv/src/pages/Home.js
+++ cv/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Navigation/>
         </div>
     );
 };
