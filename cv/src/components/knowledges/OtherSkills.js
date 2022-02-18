434de864d25f8431f12c2f96e54ee099e73d1c16
viper23 <htonnycarlos@gmail.com>
2022-02-18 21:53:09 +0200

    finish Other skills Content page

--- cv/src/components/knowledges/OtherSkills.js
+++ cv/src/components/knowledges/OtherSkills.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const OtherSkills = () => {
     return (
-        <div>
-            OtherSkills
+        <div className='otherSkills'>
+            <h3>Others Skills</h3>
+            <div className='list'>
+                <ul>
+                    <li><i className='fas fa-check-square'></i> English </li>    
+                    <li><i className='fas fa-check-square'></i> Github </li>    
+                    <li><i className='fas fa-check-square'></i> SEO </li>    
+                    <li><i className='fas fa-check-square'></i> BBB </li>    
+                </ul>
+                <ul>
+                    <li><i className='fas fa-check-square'></i> English </li>    
+                    <li><i className='fas fa-check-square'></i> Github </li>    
+                    <li><i className='fas fa-check-square'></i> SEO </li>    
+                    <li><i className='fas fa-check-square'></i> BBB </li>    
+                </ul>
+            </div>
         </div>
     );
 };
