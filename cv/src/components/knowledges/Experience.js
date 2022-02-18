9b6815f847b9ad82a43b933d1dfaf5e34b879f01
viper23 <htonnycarlos@gmail.com>
2022-02-18 21:37:56 +0200

    finish experienceContent page

--- cv/src/components/knowledges/Experience.js
+++ cv/src/components/knowledges/Experience.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 const Experience = () => {
     return (
-        <div>
-            Experience
+        <div className='experience'>
+            <h3>Experience</h3>
+            <div className='exp-1'>
+                <h4>Work on my own</h4>
+                <h5>Since 2022</h5>
+                <p>la piraterie n'est jamais finie la piraterie n'est jamais finie
+                la piraterie n'est jamais finie la piraterie n'est jamais finie 
+                la piraterie n'est jamais finie la piraterie n'est jamais finie
+                la piraterie n'est jamais finie la piraterie n'est jamais finie
+                la piraterie n'est jamais finie la piraterie n'est jamais finie
+                </p>
+            </div>
+
+            <div className='exp-2'>
+                <h4>Work on my own</h4>
+                <h5>Since 2022</h5>
+                <p>la piraterie n'est jamais finie la piraterie n'est jamais finie
+                la piraterie n'est jamais finie la piraterie n'est jamais finie 
+                la piraterie n'est jamais finie la piraterie n'est jamais finie
+                la piraterie n'est jamais finie la piraterie n'est jamais finie
+                la piraterie n'est jamais finie la piraterie n'est jamais finie
+                </p>
+            </div>
+
         </div>
     );
 };
