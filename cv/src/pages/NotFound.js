c42e675e9f709737bc31b6c2c8f1f51d601165bb
viper23 <htonnycarlos@gmail.com>
2022-02-18 14:48:07 +0200

    added notFound page

--- cv/src/pages/NotFound.js
+++ cv/src/pages/NotFound.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { NavLink} from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            NotFound
+        <div className='notFound'>
+            <div className='notFoundContent'>
+                <h3>Oops this page does not exist !</h3>
+                <NavLink exat to = "/">
+                    <i className='fas fa-home'></i>
+                    <span>Home</span>
+                </NavLink>
+            </div>
         </div>
     );
 };
