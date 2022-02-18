87189a5509f8f26739bf560c303c9ba21f0a381c
viper23 <htonnycarlos@gmail.com>
2022-02-18 22:12:23 +0200

    finish hobbies Content page

--- cv/src/components/knowledges/Hobbies.js
+++ cv/src/components/knowledges/Hobbies.js
@@ -2,8 +2,38 @@ import React from 'react';
 
 const Hobbies = () => {
     return (
-        <div>
-            Hobbies
+        <div className='hobbies'>
+            <h3>Interests</h3>
+            <ul>
+                <li className='hobby'>
+                    <i className='fas fa-runnig'></i>
+                    <span>Musculation</span>
+                </li>    
+            </ul>
+            <ul>
+                <li className='hobby'>
+                    <i className='fas fa-rapping'></i>
+                    <span>French Rap</span>
+                </li>    
+            </ul> 
+            <ul>
+                <li className='hobby'>
+                    <i className='fas fa-bitcoin'></i>
+                    <span>Cryptocurrency</span>
+                </li>    
+            </ul>
+            <ul>
+                <li className='hobby'>
+                    <i className='fas fa-hacking'></i>
+                    <span>Hacking</span>
+                </li>    
+            </ul>
+            <ul>
+                <li className='hobby'>
+                    <i className='fas fa-programming'></i>
+                    <span>Programming</span>
+                </li>    
+            </ul>
         </div>
     );
 };
