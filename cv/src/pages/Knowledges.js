70bbca6993d55599cf3d66ac0350e76bbf909fee
viper23 <htonnycarlos@gmail.com>
2022-02-18 20:09:24 +0200

    linked knwoledgesContent to knowledges page

--- cv/src/pages/Knowledges.js
+++ cv/src/pages/Knowledges.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Experience from '../components/knowledges/Experience';
+import Hobbies from '../components/knowledges/Hobbies';
+import Languages from '../components/knowledges/Languages';
+import OtherSkills from '../components/knowledges/OtherSkills';
 import Navigation from '../components/Navigation';
 
 const Knowledges = () => {
     return (
-        <div>
+        <div className='knowledges'>
             <Navigation/>
+            <div className='knowledgesContent'>
+                <Languages/>
+                <Experience/>
+                <OtherSkills/>
+                <Hobbies/>
+            </div>
         </div>
     );
 };
